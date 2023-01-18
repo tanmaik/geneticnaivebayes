9e2f9848bec48d0f7fa55ccfc15a7e3eb0a4b72c
Tanmai Kalisipudi <tanmai.kalisipudi@gmail.com>
2023-01-18 09:21:40 -0500

    Plan functions for genetic file

--- genetic.py
+++ genetic.py
@@ -1,6 +1,16 @@
 # Tanmai Kalisipudi & Griff Boehnlein
+# 1/18/23
 
-def fitness():
-    pass
+# Functions needed: 
+
+# fitness(weights)
+# generate_random_weights()
+# generate_initial_population()
+# rank_population()
+# breed(mom, dad)
+
+   
+
+    
 
 
